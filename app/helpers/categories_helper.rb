4b0d068e207387113537858415ebd3eafdb9a774
afeiship <1290657123@qq.com>
2017-07-14 20:45:02 +0800

    issue: category tree view helper with cat

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,32 +2,13 @@ module CategoriesHelper
 
     def tree_view inNode
         inNode.children.map do |child|
-            content_tag :li do |a,b|
+            content_tag :li do
+                concat content_tag :span, child.name
                 if child.children.any?
-                    tree_view(child)
+                    concat content_tag :ul , tree_view(child)
                 end
-                child.name
             end
         end.join.html_safe
     end
 
 end
-
-
-#   def display_segment( node )
-    
-#     html = "<li>"
-#     node_class = node.children.length == 0 ? "file" : "folder"
-#     html << "<span class=\"#{node_class}\">#{h(node.to_s)} </span>"
-#     html << "<ul id=\"children_of_#{h(node.sid)}\">"
-#     node.children.each{|child_node|
-      
-#       html << display_segment( child_node )
-#     }
-#     html << "</ul></li>"
-#   end
-
-# a1 = Category.find_by_id 1
-# a2 = Category.find_by_id 2
-# a3 = Category.find_by_id 3
-# SELECT "categories".* FROM "categories" WHERE "categories"."parent_id" = 1
